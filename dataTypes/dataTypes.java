e0ba4f844af7e771fb788ff3eb1266a187ca15bd
Jason <jasonetienne1@gmail.com>
2023-09-28 14:32:58 +0100

    .

---
+++ dataTypes/dataTypes.java
@@ -0,0 +1,43 @@
+package dataTypes;
+
+/**
+DATA TYPES
+
+Primitive - Integer (byte, short, int, long), Float (double, float), Char, Bool 
+
+
+
+
+ */
+
+
+class DataTypes {
+
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854L;
+
+        float f = 1.8f;
+        double d = 5.8;
+
+        char c = 'J';
+        String s = "jason";
+
+        boolean b = true;
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+
+        System.out.println(f);
+        System.out.println(d);
+
+        System.out.println(c);
+        System.out.println(s);
+
+        System.out.println(b);
+    }
+}
---
+++ literals/literals.java
@@ -0,0 +1,11 @@
+package literals;
+
+class literals {
+    
+    public static void main(String[] args) {
+        int num1 = 9;       
+
+        System.out.println(num1);
+        
+    }
+}
