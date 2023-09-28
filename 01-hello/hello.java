ce1794a5e36eeee74bdd6652e15cfd55824ed68d
Jason <jasonetienne1@gmail.com>
2023-09-28 11:40:50 +0100

    0

--- 01-hello/hello.java
+++ 01-hello/hello.java
@@ -1,5 +1,7 @@
 class hello {
-    public static void main(String a[]) {
-        System.out.println("Hello World");
-    }
+
+  public static void main(String a[]) {
+    System.out.println("Hello World");
+    System.out.println(3 + 5);
+  }
 }
---
+++ variables/vars.java
@@ -0,0 +1,15 @@
+package variables;
+
+class vars {
+
+  public static void main(String[] args) {
+    int num = 7;
+    int num2 = 8;
+    int num3 = 100;
+    int num4 = 10;
+    int result = num3 - num4;
+    System.out.println(num);
+    System.out.println(num + num2);
+    System.out.println(result);
+  }
+}
