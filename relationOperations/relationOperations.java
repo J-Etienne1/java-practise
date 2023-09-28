743866b609c6c04c1383f660c8b648c01eca7dec
Jason <jasonetienne1@gmail.com>
2023-09-28 16:03:37 +0100

    relationOperations

---
+++ relationOperations/relationOperations.java
@@ -0,0 +1,21 @@
+package relationOperations;
+
+public class relationOperations {
+    public static void main(String[] args) {
+
+
+        int x = 6;
+        int y = 5;
+
+        boolean result = x == y;
+        boolean result2 = x <= y;
+        boolean result3 = x >= y;
+        boolean result4 = x != y;
+
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+    }
+}
