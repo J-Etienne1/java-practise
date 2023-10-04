a2d69e02bdefb4856efb3650cd302e98ec7cae8e
Jason <jasonetienne1@gmail.com>
2023-10-04 10:53:48 +0100

    .

--- ifElse/ifElse.java
+++ ifElse/ifElse.java
@@ -1,7 +1,19 @@
 package ifElse;
 
 public class ifElse {
-    public static void main(String[] args) {
-        
+
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 7;
+
+    if (x > 10 && x <= 20) {
+      System.out.println("Hello");
+    } else {
+      System.out.println("Bye");
     }
+
+    if (x > y) {
+      System.out.println(x);
+    } else System.out.println(y);
+  }
 }
